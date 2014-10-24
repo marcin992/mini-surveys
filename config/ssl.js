334e70c3f8e145e15627a3b53ceeb584f49808c5
Marcin <mwincek005@gmail.com>
2014-10-24 21:08:00 +0200

    Bug fix

--- config/ssl.js
+++ config/ssl.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 
 module.exports = {
-  key: fs.readFileSync('./key.pem'),
-  cert: fs.readFileSync('./cert.crt'),
+  key: fs.readFileSync('./config/key.pem'),
+  cert: fs.readFileSync('./config/cert.crt'),
   passphrase: 'Awmsw2odoz'
 };
