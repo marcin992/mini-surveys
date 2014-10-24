46f489f5462f303f35ac37917b8473ddc5bda268
Marcin <mwincek005@gmail.com>
2014-10-24 21:16:16 +0200

    change to deploy

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@
 var config = {
   mongo: {
     development: {
-      host: "127.0.0.1",
-      port: 27017,
-      user: "",
-      password: "",
-      databaseName: "surveyDb"
+      host: "ds029960.mongolab.com",
+      port: 29960,
+      user: "admin",
+      password: "njtcmttcr",
+      databaseName: "pico-survey"
     },
     production: {
       host: 'ds029960.mongolab.com',
