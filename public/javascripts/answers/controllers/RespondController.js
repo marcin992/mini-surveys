43223e46514b6c6d4c202e4929c071581b82c573
Marcin <mwincek005@gmail.com>
2014-11-30 00:21:19 +0100

    Added multiChoice question answering

--- public/javascripts/answers/controllers/RespondController.js
+++ public/javascripts/answers/controllers/RespondController.js
@@ -11,6 +11,7 @@ answers.controller('RespondController', [
       survey: {},
       surveyCode: '',
       answers: [],
+      answeredAll: true,
       $storage: $localStorage.$default({
         responded: false
       }),
@@ -20,12 +21,18 @@ answers.controller('RespondController', [
           Surveys.getSurveyByCode($scope.surveyCode)
             .then(function (result) {
               $scope.survey = result;
-              $scope.answers = _.map($scope.survey.questions, function (q, index) {
-                return {
+              $scope.answers = _.map($scope.survey.questions, function (question, index) {
+                var object = {
                   surveyId: $scope.survey._id,
-                  questionNumber: index,
-                  respond: ''
+                  questionNumber: index
+                };
+
+                if(question.type === 'multiChoice') {
+                  object.respond = [];
+                } else {
+                  object.respond = '';
                 }
+                return object;
               });
             });
         }, 0);
@@ -33,12 +40,45 @@ answers.controller('RespondController', [
       },
 
       saveAnswers: function () {
-        Answers.saveAnswers($scope.answers)
-          .then(function (result) {
-            $scope.$storage[$scope.surveyCode] = {
-              responded: true
+        if($scope.checkIfAllAnswered()) {
+          Answers.saveAnswers($scope.answers)
+            .then(function (result) {
+              $scope.$storage[$scope.surveyCode] = {
+                responded: true
+              }
+            });
+        }
+
+      },
+
+      checkIfAllAnswered: function() {
+        $scope.answeredAll = true;
+        for(var i = 0; i < $scope.answers.length; i++) {
+          var answer = $scope.answers[i];
+          if(_.isArray(answer.respond)) {
+            if(answer.respond.length === 0) {
+              $scope.answeredAll = false;
+              break;
             }
-          });
+          } else {
+            if(answer.respond === '') {
+              $scope.answeredAll = false;
+              break;
+            }
+          }
+        }
+
+        return $scope.answeredAll;
+      },
+
+      toggleSelection: function(questionNumber, answer) {
+        var index = $scope.answers[questionNumber].respond.indexOf(answer);
+
+        if(index > -1) {
+          $scope.answers[questionNumber].respond.splice(index, 1);
+        } else {
+          $scope.answers[questionNumber].respond.push(answer);
+        }
       },
 
       dd: function (q) {
--- routes/respondentRoutes.js
+++ routes/respondentRoutes.js
@@ -1,9 +1,10 @@
 var MessageSender = require('../utils/MessageSender');
 var Message = require('../utils/Messages');
+var _ = require('lodash-node');
 
-module.exports = function(answerProvider, answerMiner) {
+module.exports = function (answerProvider, answerMiner) {
   return {
-    surveyView: function(req, res) {
+    surveyView: function (req, res) {
       var surveyCode = req.params.surveyCode;
 
       res.render('respondentPage', {
@@ -11,22 +12,35 @@ module.exports = function(answerProvider, answerMiner) {
       });
     },
 
-    saveAnswer: function(req, res) {
+    saveAnswer: function (req, res) {
       var answers = req.body.answers;
 
+      _.each(req.body.answers, function (answer, index) {
+        if (_.isArray(answer.respond)) {
+          answers.splice(index, 1);
+          _.each(answer.respond, function(respond) {
+            answers.push({
+              surveyId: answer.surveyId,
+              questionNumber: answer.questionNumber,
+              respond: respond
+            });
+          });
+        }
+      });
+
       answerProvider.saveAnswers(answers)
-        .then(function(result) {
+        .then(function (result) {
           MessageSender.sendJsonObject(res, result);
-        }, function(err) {
+        }, function (err) {
           MessageSender.sendDatabaseError(res, err);
         })
     },
 
-    getAnswers: function(req, res) {
+    getAnswers: function (req, res) {
       answerMiner.countAnswers(req.params.surveyId)
-        .then(function(result) {
+        .then(function (result) {
           MessageSender.sendJsonObject(res, result);
-        }, function(err) {
+        }, function (err) {
           MessageSender.sendDatabaseError(res, err);
         });
       //answerProvider.getAnswers(req.params.surveyId)
@@ -37,11 +51,11 @@ module.exports = function(answerProvider, answerMiner) {
       //  });
     },
 
-    deleteAnswers: function(req, res) {
+    deleteAnswers: function (req, res) {
       answerProvider.deleteAnswers(req.params.surveyId, req.body.questionNumber)
-        .then(function() {
+        .then(function () {
           MessageSender.sendMessage(res, "Answers deleted");
-        }, function(err) {
+        }, function (err) {
           MessageSender.sendError(res, err);
         });
     }
